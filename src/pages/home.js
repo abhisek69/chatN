a57e61d88571c04359b3110b15901ac12cb808ea
abhisek69 <rohan.x360@hotmail.com>
2024-05-29 23:57:08 +0530

    menu designed

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,21 @@
-import React , { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import { ReactComponent as DarkIcon } from '../../src/components/icons/dark.svg';
 import { ReactComponent as LightIcon } from '../../src/components/icons/light.svg';
-import { auth , db } from "../components/firebase";
-import { doc , getDoc } from "firebase/firestore";
+import { auth, db } from "../components/firebase";
+import { doc, getDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import mypic  from "./mypic.jpg";
+import Chats from "./chats";
 const Home = () => {
     const navigate = useNavigate();
-    const {  logout } = useAuth(null);
+    const { logout } = useAuth(null);
     const [toggletheme, settoggletheme] = useState(false);
-    const [User , SetUser]= useState()
+    const [User, SetUser] = useState()
     const ToggleDark = () => {
         settoggletheme(!toggletheme);
-        localStorage.setItem('theme', JSON.stringify(toggletheme?'light':'dark'));
+        localStorage.setItem('theme', JSON.stringify(toggletheme ? 'light' : 'dark'));
         toggletheme ? console.log('light') : console.log('dark');
     }
 
@@ -31,15 +33,15 @@ const Home = () => {
             console.log("User not authenticated");
         }
     };
-    
-    
-    const handleLogout = async ()=>{
-     try {
-        await logout(User.userName);
-        navigate("/")
-     } catch (error) {
-        navigate("/")
-     }
+
+
+    const handleLogout = async () => {
+        try {
+            await logout(User.userName);
+            navigate("/")
+        } catch (error) {
+            navigate("/")
+        }
     }
     useEffect(() => {
         const savedTheme = JSON.parse(localStorage.getItem('theme'));
@@ -47,22 +49,42 @@ const Home = () => {
             settoggletheme(savedTheme === 'dark');
         }
         getUserData();
-        
-      }, []);
+
+    }, []);
 
     return (
-        <div className={`home h-screen ${toggletheme ? 'dark' : 'light'} flex`}>
-            <div className="absolute flex items-center justify-around top-4 right-4 " >
-                <button className="btn p-2 rounded-full" onClick={ToggleDark}>
-                {toggletheme  ? <LightIcon style={{height:20,width:20}}  /> : <DarkIcon style={{height:20,width:20}}  />}
-                </button>
-                <button onClick={handleLogout} className="btn p-2 rounded-2xl m-2">logout</button>
+        <div className={`home h-screen ${toggletheme ? 'dark' : 'light'}`}>
+            
+            <div className="menu w-auto h-full flex flex-col">
+                <div className="flex justify-center">
+                    {User ? (<div className=" flex justify-center items-center p-4  text-white group w-full uppermenu"  style={{ height: 'fit-content' }}>
+                    <div className="" >
+                            <img className="mr-4 border rounded-full" style={{height:38,width:38}} src={mypic} alt="" />
+                        </div>
+                        <h3 className="text-xl">{User.userName}</h3>
+                        
+                        <div className="absolute top-20 p-4 rounded-lg shadow-lg hidden group-hover:block" style={{background:'purple'}}>
+                            <p className="text-sm text-gray-300">{User.email}</p>
+                            <p className="text-sm text-gray-300">{User.number}</p>
+                        </div>
+                    </div>) : (<div></div>)}
+                    
+                </div>
+                <Chats />
+                <div className="flex-grow"></div>
+                <div className="flex items-center justify-center p-4 uppermenu">
+                    <button className="btn p-2 mr-4 rounded-full" onClick={ToggleDark}>
+                        {toggletheme ? (
+                            <LightIcon style={{ height: 20, width: 20 }} />
+                        ) : (
+                            <DarkIcon style={{ height: 20, width: 20 }} />
+                        )}
+                    </button>
+                    <button onClick={handleLogout} className="btn p-2 rounded-2xl m-2" style={{width:"100px"}}>
+                        logout
+                    </button>
+                </div>
             </div>
-         {User?(<div className="border p-4 m-2" style={{height:'fit-content'}}>
-            <h3>Welcome {User.userName}</h3>
-            <p>{User.email}</p>
-            <p>{User.number}</p>
-         </div>):(<div><p>no user details present</p></div>)}
         </div>
     );
 }
